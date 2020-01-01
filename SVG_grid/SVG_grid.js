ebd098b5082bdcf9b863322de800580901b727c6
Guillaume Roux <guiguionthenet@free.fr>
2020-01-01 19:17:53 +0100

    L'ajout de la classe hide est à corriger.

--- SVG_grid/SVG_grid.js
+++ SVG_grid/SVG_grid.js
@@ -24,6 +24,11 @@ var maze_test = [[6,14,10,10,8,6,10,10,10,14,10,10,12,6,10,8,6,14,12,6,10,10,8,6
 				 [5,5,6,12,3,12,5,3,12,3,9,6,10,12,3,12,3,12,3,10,12,5,4,5,1,3,10,12,6,13],
 				 [3,11,9,3,10,9,3,10,11,10,10,9,2,11,8,3,10,11,10,8,3,11,9,3,10,10,10,11,9,1]];
 
+const up_bit = 0b0001;
+const right_bit = 0b0010;
+const down_bit = 0b0100;
+const left_bit = 0b100;
+
 svg_element = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 svg_element.setAttribute('width', 600);
 svg_element.setAttribute('height', 600);
@@ -42,6 +47,7 @@ var p = maze_test[0].length;
 
 function add_grid_element(parent, x_off_first, y_off_first, x_off, y_off, w, h, i, j, classList=[]) {
 	let element = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+	
 	element.setAttribute('x', x_off_first + (x_off + w) * j);
 	element.setAttribute('y', y_off_first + (y_off + h) * i);
 	element.setAttribute('width', w);
@@ -50,6 +56,7 @@ function add_grid_element(parent, x_off_first, y_off_first, x_off, y_off, w, h, 
 	for(let c of classList) {
 		element.classList.add(c);
 	}
+	element.classList.add('c' + i + '-' + j);
 	
 	parent.appendChild(element);		
 }
@@ -98,4 +105,42 @@ add_grid_element(svg_element,
 				 0, 0, row_width, col_height, col_width, row_height, n, p,
 				 ['corner']);
 
+// remove proper walls
+function make_maze() {
+	for(let i = 0; i < n; i++) {
+		for(let j = 0; j < p; j++) {
+			let cell = maze_test[i][j];
+			let el = null;
+
+			if(cell & up_bit) {
+				el = document.querySelector('.row.c' + i + '-' + j);
+				if(el) {
+					el.classList.add('hide');
+				}
+				console.log('up', el);
+			}
+			if(cell & right_bit) {
+				el = document.querySelector('.col.c' + i + '-' + (j + 1));
+				if(el) {
+					el.classList.add('hide');
+				}
+				console.log('right', el);
+			}
+			if(cell & down_bit) {
+				el = document.querySelector('.row.c' + (i + 1) + '-' + j);
+				if(el) {
+					el.classList.add('hide');
+				}
+				console.log('down', el);
+			}
+			if(cell & left_bit) {
+				el = document.querySelector('.col.c' + i + '-' + j);
+				if(el) {
+					el.classList.add('hide');
+				}
+				console.log('left', el);
+			}
+		}
+	}
+}
 document.body.appendChild(svg_element);
