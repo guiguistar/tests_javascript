854b0022e8cc29f1a79f6b1e9f16a2ac253c1dcc
Guillaume Roux <guillaume.roux.math@free.fr>
2020-01-02 16:08:16 +0100

    Essais sur les promesses en Javascript

--- SVG_grid/SVG_grid.js
+++ SVG_grid/SVG_grid.js
@@ -24,6 +24,14 @@ var maze_test = [[6,14,10,10,8,6,10,10,10,14,10,10,12,6,10,8,6,14,12,6,10,10,8,6
 				 [5,5,6,12,3,12,5,3,12,3,9,6,10,12,3,12,3,12,3,10,12,5,4,5,1,3,10,12,6,13],
 				 [3,11,9,3,10,9,3,10,11,10,10,9,2,11,8,3,10,11,10,8,3,11,9,3,10,10,10,11,9,1]];
 
+maze_test2 = [[0b0010, 0b1010, 0b1110, 0b1000, 0b0100],
+			  [0b0100, 0b0010, 0b1111, 0b1010, 0b1101],
+			  [0b0011, 0b1010, 0b1001, 0b0010, 0b1001]]
+
+maze_test3 = [[0b0010, 0b1010, 0b1100],
+			  [0b0100, 0b0010, 0b1101],
+			  [0b0011, 0b1010, 0b1001]]
+
 class Mazer {
 	constructor(maze) {
 		// class field not currently supported
@@ -36,6 +44,8 @@ class Mazer {
 		this.row_height = 4;
 		this.col_width = 4;
 		this.col_height = 15;
+
+		this.maze = maze;
 		
 		this.n = maze.length;
 		this.p = maze[0].length;
@@ -124,7 +134,7 @@ class Mazer {
 	make_maze(grid) {
 		for(let i = 0; i < this.n; i++) {
 			for(let j = 0; j < this.p; j++) {
-				let cell = maze_test[i][j];
+				let cell = this.maze[i][j];
 				let [row, col, corner] = grid[i][j];
 
 				if(cell & this.up_bit) {
@@ -141,7 +151,7 @@ class Mazer {
 	create_path(svg_el) {
 		for(let i = 0; i < this.n; i++) {
 			for(let j = 0; j < this.p; j++) {
-				let cell = maze_test[i][j];
+				let cell = this.maze[i][j];
 
 				let center = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 				let cx = j * (this.col_width + this.row_width) + this.col_width + this.row_width / 2;
@@ -171,6 +181,31 @@ class Mazer {
 	}
 }
 
-m = new Mazer(maze_test);
+
+function request_new_maze (rows=10, cols=10) {
+	return new Promise(function (resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'http://www.lespursetdurs.fr/maze/?rows=' + rows + '&cols=' + cols +'&json');
+		xhr.onload = function () {
+			if (this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			}
+		};
+		xhr.onerror = function () {
+			reject({
+				status: this.status,
+				statusText: xhr.statusText
+			});
+		};
+		xhr.send();
+	});
+}
+
+m = new Mazer(maze_test3);
 
 document.body.appendChild(m.createSVG());
